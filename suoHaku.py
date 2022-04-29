c21a77c32c7168c2ef48a1a69ad061722aa5dde4
Julius Maylett <juliusmaylett@gmail.com>
2022-04-29 14:46:01 +0300

    added gitignore

--- suoHaku.py
+++ suoHaku.py
@@ -2,20 +2,22 @@
 # Ohjelma hakee XML-muodossa kiinteistörajoja annettujen rajojen sisältä. 
 # Lue README tarkkaan
 
+from asyncio.windows_events import NULL
 import os
 import json
 import requests
 from geojson import FeatureCollection
 
-# Määritä työtiedosto, joka on samassa hakemistossa tämän ajotiedoston kanssa.
-fileName = "testi.geojson"
+# Määritä työtiedoston nimi, joka on read-hakemistossa.
+filename = "uudetAlueetSimplifiedBuffer"
+
+file = "read/"+ filename +".geojson"
+NEXT_INDEX, i = 0, 1
 
 def CQLString(geometry):
     CQLStatement = "CQL_FILTER=INTERSECTS(geometry,POLYGON(("
     for point in geometry:
-        lon = point[0]
-        lat = point[1]
-        CQLStatement = CQLStatement + str(lon) + "%20" + str(lat) + ","
+        CQLStatement = CQLStatement + str(int(point[0])) + "%20" + str(int(point[1])) + ","
     CQLStatement = CQLStatement[:-1]
     CQLStatement = CQLStatement + ")))"
     return CQLStatement
@@ -27,32 +29,34 @@ def CPXML(geometry):
     "TYPENAMES=cp:CadastralParcel&TYPENAME=cp:CadastralParcel&STARTINDEX=0&COUNT=200000&SRSNAME=EPSG:3067&" + cql_String
     r = requests.get(CPURL) # Haku on erittäin hidas, kärsivällisyyttä
     return r.text
+    
 
-with open(fileName) as f:
-    data = json.load(f)
+def writeXML(fid, xml, multipart=NULL):
+    if multipart == NULL: fileString = 'output/{}.xml'.format(fid)
+    else: fileString = 'output/{}_{}.xml'.format(fid, NEXT_INDEX)
+    with open(fileString, 'w') as f:
+        f.write(xml)
+        NEXT_INDEX =+ 1
 
+## STARTING ##
+with open(file) as f: data = json.load(f)
 feature_collection = FeatureCollection(data)
-
-i = 0
 
 print("Started")
 
-# Huolehditaan, että output-kansio on olemassa
 if not os.path.isdir('./output'):
+    # Huolehditaan, että output-kansio on olemassa
     os.mkdir('output') 
 
 for feature in feature_collection['features']:
+   
+    if (feature['geometry']['type'] == 'MultiPolygon'): 
+        for poly in feature['geometry']['coordinates']: 
+            writeXML(feature['properties']['fid'], CPXML(poly[0]), NEXT_INDEX)
+        NEXT_INDEX = 0
 
-    print("Collecting CP data from NLS...")
-    # Haetaan geometria, ja määritellään tiedoston nimeksi fid-tunniste
-    xml = CPXML(feature['geometry']['coordinates'][0])
-    fid = feature['properties']['fid']
-
-    with open('output/{}.xml'.format(fid), 'w') as f:
-        f.write(xml)
-
-    i += 1
+    if (feature['geometry']['type'] == 'Polygon'): writeXML(feature['properties']['fid'], CPXML(feature['geometry']['coordinates'][0]))
     print("Completed:" , i , "/" , len(feature_collection['features']))
-
+    i =+ 1
 
 print("Finished")
