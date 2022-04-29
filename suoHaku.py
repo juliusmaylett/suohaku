33d13e2b32f941de3bd5661c68858f1b011dc6ad
Julius Maylett <juliusmaylett@gmail.com>
2022-04-29 14:59:06 +0300

    changes

--- suoHaku.py
+++ suoHaku.py
@@ -2,13 +2,12 @@
 # Ohjelma hakee XML-muodossa kiinteistörajoja annettujen rajojen sisältä. 
 # Lue README tarkkaan
 
-from asyncio.windows_events import NULL
 import os
 import json
 import requests
 from geojson import FeatureCollection
 
-# Määritä työtiedoston nimi, joka on read-hakemistossa.
+# Määritä työtiedoston nimi, joka on read-hakemistossa projektin juuressa
 filename = "uudetAlueetSimplifiedBuffer"
 
 file = "read/"+ filename +".geojson"
@@ -25,14 +24,16 @@ def CQLString(geometry):
 def CPXML(geometry):
     # Tehdään intersectiohaku annetun geometrian avulla käyttäen CQLSTATEMENTIA
     cql_String = CQLString(geometry)
-    CPURL = "https://inspire-wfs.maanmittauslaitos.fi/inspire-wfs/cp/wfs?srsName=EPSG:3067&SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&"\
-    "TYPENAMES=cp:CadastralParcel&TYPENAME=cp:CadastralParcel&STARTINDEX=0&COUNT=200000&SRSNAME=EPSG:3067&" + cql_String
+    CPURL = "https://inspire-wfs.maanmittauslaitos.fi/inspire-wfs/cp/wfs?"\
+    "srsName=EPSG:3067&SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&"\
+    "TYPENAMES=cp:CadastralParcel&TYPENAME=cp:CadastralParcel&STARTINDEX=0&"\
+    "COUNT=200000&SRSNAME=EPSG:3067&" + cql_String
     r = requests.get(CPURL) # Haku on erittäin hidas, kärsivällisyyttä
     return r.text
     
 
-def writeXML(fid, xml, multipart=NULL):
-    if multipart == NULL: fileString = 'output/{}.xml'.format(fid)
+def writeXML(fid, xml, NEXT_INDEX=None):
+    if NEXT_INDEX == None: fileString = 'output/{}.xml'.format(fid)
     else: fileString = 'output/{}_{}.xml'.format(fid, NEXT_INDEX)
     with open(fileString, 'w') as f:
         f.write(xml)
@@ -48,7 +49,7 @@ if not os.path.isdir('./output'):
     # Huolehditaan, että output-kansio on olemassa
     os.mkdir('output') 
 
-print(len(feature_collection['features']) + " areas to handle...")
+#print(len(feature_collection['features']) + " areas to handle...")
 
 for feature in feature_collection['features']:
    
@@ -56,7 +57,6 @@ for feature in feature_collection['features']:
         for poly in feature['geometry']['coordinates']: 
             writeXML(feature['properties']['fid'], CPXML(poly[0]), NEXT_INDEX)
         NEXT_INDEX = 0
-
     if (feature['geometry']['type'] == 'Polygon'): writeXML(feature['properties']['fid'], CPXML(feature['geometry']['coordinates'][0]))
     print("Completed:" , i , "/" , len(feature_collection['features']))
     i =+ 1
